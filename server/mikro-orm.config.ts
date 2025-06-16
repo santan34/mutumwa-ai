35a77b6e2a719f1c64402a263296242bd675a356
tnmush <tnmush@yahoo.com>
2025-06-16 14:34:24 +0200

    refactor: migrate from Prisma to MikroORM for database management
    - Updated package.json to remove Prisma dependencies and add MikroORM dependencies.
    - Deleted Prisma client initialization file and migration files.
    - Removed Prisma schema file and migration lock file.
    - Added MikroORM configuration file for PostgreSQL.
    - Updated tsconfig.json to enable experimental decorators and emit decorator metadata.

---
+++ server/mikro-orm.config.ts
@@ -0,0 +1,14 @@
+import { Options } from "@mikro-orm/core";
+import { PostgreSqlDriver } from "@mikro-orm/postgresql";
+
+const config: Options<PostgreSqlDriver> = {
+  entities: ["./dist/entities"],
+  dbName: process.env.DB_NAME,
+  driver: PostgreSqlDriver,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  entitiesTs: ["./src/entities"],
+  debug: true,
+};
+
+export default config;
